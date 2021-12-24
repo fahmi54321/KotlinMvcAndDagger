6a098ae333ae389af24ef84d05ce1c2abd40c7aa
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-12-24 22:21:24 +0700

    banyak view model

---
+++ app/src/main/java/com/android/a2mvcdandagger/common/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,23 @@
+package com.android.a2mvcdandagger.common.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.android.a2mvcdandagger.screens.viewmodel.MyViewModel
+import com.android.a2mvcdandagger.screens.viewmodel.MyViewModel2
+import java.lang.RuntimeException
+import javax.inject.Inject
+import javax.inject.Provider
+
+//todo 1 (next MyViewModel)
+class ViewModelFactory @Inject constructor(
+    private val myViewModelProvider: Provider<MyViewModel>,
+    private val myViewModelProvider2: Provider<MyViewModel2>
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when (modelClass) {
+            MyViewModel::class.java -> myViewModelProvider.get() as T
+            MyViewModel2::class.java -> myViewModelProvider2.get() as T
+            else -> throw RuntimeException("unsupported viewmodel type: $modelClass")
+        }
+    }
+}
