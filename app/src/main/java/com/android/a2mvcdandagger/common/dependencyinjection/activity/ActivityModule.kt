8e67838d7c3cbf607baf8b179576ee0c0a69508f
fahmi <fahmiabdulaziz44@gmail.com>
2024-01-16 21:00:53 +0700

    update code

--- app/src/main/java/com/android/a2mvcdandagger/common/dependencyinjection/activity/ActivityModule.kt
+++ app/src/main/java/com/android/a2mvcdandagger/common/dependencyinjection/activity/ActivityModule.kt
@@ -2,6 +2,8 @@ package com.android.a2mvcdandagger.common.dependencyinjection.activity
 
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.android.a2mvcdandagger.screens.common.dialogs.DialogsNavigator
+import com.android.a2mvcdandagger.screens.common.dialogs.DialogsNavigatorImpl
 import com.android.a2mvcdandagger.screens.common.navigator.ScreenNavigator
 import com.android.a2mvcdandagger.screens.common.navigator.ScreenNavigatorImpl
 import dagger.Binds
@@ -13,6 +15,9 @@ abstract class ActivityModule {
 
     @Binds
     abstract fun screenNavigator(screenNavigatorImpl: ScreenNavigatorImpl): ScreenNavigator
+
+    @Binds
+    abstract fun dialogNavigator(dialogNavigator: DialogsNavigatorImpl): DialogsNavigator
 
     companion object{
         @Provides
